f316a49c66eb18130b8a8a1cae771b7f67f20de4
arthleo <arthurleonard05225@gmail.com>
2026-07-21 16:51:16 +1000

    Fixed wiring on routes

--- Backend_PMS/src/app.js
+++ Backend_PMS/src/app.js
@@ -21,6 +21,7 @@ app.use('/api/projects', projectRoutes);
 app.use('/api/tasks', taskRoutes);app.use('/api/profile', require('./routes/profileRoutes'));
 app.use('/api/allocations', require('./routes/allocationRoutes'));
 app.use('/api/notifications', require('./routes/notificationRoutes'));
+app.use('/api/assessments', require('./routes/assessmentRoutes'));
 app.use(notFound);
 app.use(errorHandler);
 
